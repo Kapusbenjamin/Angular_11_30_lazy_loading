fc1d9a2d564e302657e8a1e9905dc8958be68de9
Kapusbenjamin <47866619+Kapusbenjamin@users.noreply.github.com>
2022-12-21 13:11:19 +0100

    Added table to collections

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
 
+  data: any[] = [
+    {
+      name: "James", age: 24, job: "Designer", employed: true
+    },
+    {
+      name: "Jill", age: 27, job: "Engineer", employed: true
+    },
+    {
+      name: "Elyse", age: 22, job: "Designer", employed: false
+    }
+  ]
+  headers: any[] = [
+    {
+      key: "employed", label: "Has a job?"
+    },
+    {
+      key: "name", label: "Name"
+    },
+    {
+      key: "age", label: "Age"
+    },
+    {
+      key: "job", label: "Job"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
